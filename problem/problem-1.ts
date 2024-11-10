b1f99771f78bc154bf5a4de319f72fa4b7ec5f52
Tonmoy-Rahman-Turjo <tonmoy9828@gmai.com>
2024-11-11 00:14:24 +0600

    solve problem-1

---
+++ problem/problem-1.ts
@@ -0,0 +1,18 @@
+{
+    {
+
+
+        function sumArray(numbers:number[]): number{
+          let count = 0;
+          for(let i=0; i<numbers.length; i++){
+            count += numbers[i]
+          }
+          return count
+        }
+          console.log(sumArray([1,2,3,4,5.7,8,9]))
+        
+        }
+
+
+
+}
